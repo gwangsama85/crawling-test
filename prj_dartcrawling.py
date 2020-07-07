68d86f4890d9750901a631dd9f23aa370b2c2122
gwangsama85 <gwangsama85@gmail.com>
2020-07-08 00:58:12 +0900

    prj_dartcrawling_commit_200707

--- prj_dartcrawling.py
+++ prj_dartcrawling.py
@@ -3,17 +3,44 @@ import pandas as pd
 from io import BytesIO
 
 u_a = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
+rcp_no = '20190814002218'
+dcm_no = '6846651'
+url_1908 = "http://dart.fss.or.kr/pdf/download/excel.do?rcp_no="+ rcp_no + "&dcm_no=" + dcm_no +"&lang=ko"
 url_2005 = "http://dart.fss.or.kr/pdf/download/excel.do?rcp_no=20200515002179&dcm_no=7343256&lang=ko"
-url_1908 = "http://dart.fss.or.kr/pdf/download/excel.do?rcp_no=20190814002218&dcm_no=6846651&lang=ko"
 pocket = ["연결 재무상태표","연결 자본변동표","연결 손익계산서","연결 포괄손익계산서", "연결 자본변동표", "연결 현금흐름표"]
 
-for period,url in zip(["1908","2005"],[url_1908,url_2005]) :
+
+
+"""for period,url in zip(["1908","2005"],[url_1908,url_2005]) :
     resp = requests.get(url,headers ={"user-agent" : u_a })
     for sheet in ["기본정보", "연결 재무상태표","연결 손익계산서", "연결 포괄손익계산서"] :
         table = BytesIO(resp.content)
         data = pd.read_excel(table, sheet_name=sheet, skiprows=5)
         data.to_csv(period+sheet + ".csv", encoding="euc-kr")
 print(url_1908)
+"""
+def download_excel(url1,period,company) :
+    for period, url in zip([period], [url1]):
+        resp = requests.get(url1, headers={"user-agent": u_a})
+        for sheet in ["기본정보", "연결 재무상태표", "연결 손익계산서", "연결 포괄손익계산서"]:
+            table = BytesIO(resp.content)
+            data = pd.read_excel(table, sheet_name=sheet, skiprows=5)
+            data.to_csv(company+period + sheet + ".csv", encoding="euc-kr")
+print(url_1908)
+
+def download_excel_2(rcp_no, dcm_no, period,company) :
+    url1 = "http://dart.fss.or.kr/pdf/download/excel.do?rcp_no=" + rcp_no + "&dcm_no=" + dcm_no + "&lang=ko"
+    resp = requests.get(url1, headers={"user-agent": u_a})
+    for sheet in ["기본정보", "연결 재무상태표", "연결 손익계산서", "연결 포괄손익계산서"]:
+        table = BytesIO(resp.content)
+        data = pd.read_excel(table, sheet_name=sheet, skiprows=5)
+        data.to_csv(company+period + sheet + ".csv", encoding="euc-kr")
+
+download_excel(url_1908,"1908_","삼성전자")
+download_excel(url_2005,"2005_","삼성전자")
+
+download_excel_2(rcp_no,dcm_no,"1908_","삼성전자")
+
 
 """data=pd.read_excel(table, s
 heet_name="기본정보",skiprows=5)
